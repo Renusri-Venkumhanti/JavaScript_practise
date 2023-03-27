fe7e858dd57db996dd80940b67277e7c0d732c37
Renusri Venkumahanti <renusriv54@gmail.com>
2023-03-27 22:25:30 +0530

    Optimised find number game code

--- Guess Number Game/starter/script.js
+++ Guess Number Game/starter/script.js
@@ -3,28 +3,20 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 document.querySelector('.score').textContent = score;
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = document.querySelector('.guess').value;
+  const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     document.querySelector('.message').textContent = '⛔No Number!';
-  } else if (guess == secretNumber) {
+  } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     if (document.querySelector('.highscore').textContent < score)
       document.querySelector('.highscore').textContent = score;
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈Too high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💣 You Lost ';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉Too Low';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈Too high' : '📉Too Low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
